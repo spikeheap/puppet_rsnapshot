1ed3de251b5dc56b5023ba27bff7069ceb0fab2a
root <root@puppet.nmi.uk.com>
2013-08-08 14:10:22 +0000

    rsnapshot produces sane backups of local clients

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,4 +1,4 @@
-class rsnapshot::client{
-  $rsnapshot_dirs = hiera('rsnapshot_directories',[])
-  rsnapshot::directory{$rsnapshot_dirs: }
+class rsnapshot::client( $dirs = [] ){
+  #$rsnapshot_dirs = hiera('rsnapshot_directories',[])
+  rsnapshot::directory{$dirs: }
 }
