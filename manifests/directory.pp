d318a1c02e11fea825fc0daac948a15c45609ee2
root <root@puppet.nmi.uk.com>
2013-08-08 10:48:26 +0000

    Rsnapshot local working, still WIP needing users

--- manifests/directory.pp
+++ manifests/directory.pp
@@ -4,14 +4,24 @@ define rsnapshot::directory {
   # TODO backup user SSH keys
   # TODO require SSH
   
-  #backup	/home/		localhost/
-  #backup	/etc/		localhost/
-  #backup	/usr/local/	localhost/
-  
+  # Add a slash if it's been omitted (otherwise rsnapshot just won't work)
+  if $name !~ /\/$/ {
+    $directory = "${name}/"
+  }else{
+    $directory = "${name}"
+  }
 
-  concat::fragment { "rsnapshot_fragment_$name":
+  # Build a single directory fragment for rsnapshot.conf.
+  # The format is:
+  #   backup	DIRECTORY	BACKUP_DEST
+  # Both DIRECTORY and BACKUP_DEST must be followed with a slash.
+  #   DIRECTORY		The source for the backup, e.g. backupuser@example.com:/etc/puppet/
+  #
+  #   BACKUP_DEST	The destination for the backup on the rsnapshot server, e.g. example.com/
+  #			Note that this doesn't include the path from DIRECTORY.
+  concat::fragment { "rsnapshot_fragment_$directory":
     target  => '/etc/rsnapshot.conf',
-    content => "backup	$name	${::fqdn}/\n",
+    content => "backup	$directory	${::fqdn}/\n",
     order   => '15',
   }
 }
